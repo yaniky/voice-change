a2d03c1de6bf51259fe4bbaf9afbafa664e473ea
Ethan Zhuo <ethan.zhuo@viviu.com>
2021-04-26 17:15:59 +0800

    feat: add girlvoice

--- src/index.js
+++ src/index.js
@@ -24,3 +24,27 @@ document.getElementById("start1").onclick = async function() {
 document.getElementById("stop1").onclick = async function() {
     audioPlayer.stop();
 };
+
+const voiceView1 = new VoiceView({
+    container: document.getElementById("view2"),
+    yMul: 50
+});
+
+const audioPlayer1 = new Audioplayer({
+    playerWay: Audioplayer.PLAY_WAY.BUFFER,
+    needChange: false,
+    voiceView: voiceView1
+});
+
+document.getElementById("start2").onclick = async function() {
+    const hellowBuffer = await fetch("/static/girl.wav").then(res=> res.arrayBuffer());
+
+    audioPlayer1
+        .setVoiceFreMul(2)
+        .setPeaking(440);
+    await audioPlayer1.setBuffer(hellowBuffer);
+    audioPlayer1.ready().play();
+};
+document.getElementById("stop2").onclick = async function() {
+    audioPlayer1.stop();
+};
